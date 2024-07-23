bf5f21f9a6dddbd116074f0c0241ab41fa3c2ff1
Jayanth <125460226+JayanthSL@users.noreply.github.com>
2024-07-23 17:53:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -111,4 +111,6 @@ def on_join(data):
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=12000, debug=True,allow_unsafe_werkzeug=True)
+    # For development use only:
+    socketio.run(app, host="0.0.0.0", port=12000, debug=False, allow_unsafe_werkzeug=True)
+
